ebb2ec7f6a5cc9ec07231ea33f3392d4b28e29f6
tom tkeogh <thk11@aber.ac.uk>
2014-03-08 19:00:08 +0000

    reads both files as planned, just compare now.

--- CS225 Own Parser/src/Assignment/Stream.java
+++ CS225 Own Parser/src/Assignment/Stream.java
@@ -10,23 +10,25 @@ public class Stream {
 
 	private ArrayList<GGA> ggas;
 	private ArrayList<Satellite> sats;
+	private ArrayList<Integer> satids;
+	private int failed = 0;
+
 
 
 	public Stream(){
 
 		ggas = new ArrayList<GGA>();	
 		sats = new ArrayList<Satellite>();
+		satids = new ArrayList<Integer>();
 
 		readin();
-		//check();
-		for(int i =0;i<sats.size();i++){
-			System.out.println(sats.get(i).getId());
-		}
+		// check();
+
 	}
 
 	public void readin(){
 
-		File file = new File("gps_1.dat");		
+		File file = new File("gps_2.dat");		
 
 		BufferedReader br = null;
 
@@ -46,7 +48,7 @@ public class Stream {
 
 					int pass = countReliable();
 
-					if(pass>3){
+					if(pass>=3){
 
 						GGA newGGA = new GGA();
 
@@ -62,18 +64,98 @@ public class Stream {
 						newGGA.setSeperation(Double.parseDouble(p[11]));
 
 						ggas.add(newGGA);
+						System.out.println("----------WOOOOOOH YEAH ADDED   " + ggas.size() + " ---------");
+						for(int l =0;l<sats.size();l++){
+							//System.out.println("Using Satellite "+ sats.get(l).getId() + " Logged SNR : "+ sats.get(l).getSnr());
+
+						}
+					}
+					else{
+						System.out.println("================FAILED HERE ======================");
+						for(int l =0;l<sats.size();l++){
+							System.out.println("Using Satellite "+ sats.get(l).getId() + " Logged SNR : "+ sats.get(l).getSnr());
+
+						}
+						failed++;
+						System.out.println(failed);
 					}
 
 				}
 				else if(p[0].equalsIgnoreCase("$GPGSV")){
 
-
+					satids.clear();
 					String gsv[] = sCurrentLine.toString().split(",");
-					for(int i =0;i<gsv.length;i++){
+					String[] finalone = gsv[gsv.length-1].toString().split("\\*");
+					gsv[gsv.length-1] = finalone[0];        //removes checksum so just the satellite snr left.
+					
+					int expectedsats   = 0;
+					int lineno = Integer.parseInt(gsv[2]);
+					int satelliteno = Integer.parseInt(gsv[3]);
 
-						//System.out.println(gsv[i]+" "+ i);			
 
+					int reaminingsats = satelliteno - ((lineno-1) * 4);
+
+					if(reaminingsats >=4){
+						expectedsats = 4;
 					}
+
+					for(int i =0;i<expectedsats;i++){
+						int position = 4 *(i+1);
+						satids.add(Integer.parseInt(gsv[position]));
+					}
+					
+					for(int i =0;i<satids.size();i++){
+						int currentsat = satids.get(i);
+						for(int j =0;j<sats.size();j++){
+							int cur = sats.get(j).getId();	
+							if(currentsat == cur){
+								int snrposition = ((i+1)*4)+3;
+								int snr = Integer.parseInt(gsv[snrposition]);
+								sats.get(j).setSnr(snr);
+							}
+							
+						
+						}
+						
+					}
+					
+
+					/*first = Integer.parseInt(gsv[4]);
+					second = Integer.parseInt(gsv[8]);
+					third = Integer.parseInt(gsv[12]);
+					fourth = Integer.parseInt(gsv[16]);
+
+
+					for(int i =0;i<sats.size();i++){
+
+						int curr = sats.get(i).getId();
+
+						if(curr == first){
+							if(!gsv[7].isEmpty()){
+								sats.get(i).setSnr(Integer.parseInt(gsv[7]));
+								//	System.out.println("Satellite " + curr + " set to  " + gsv[7]);
+							}
+						}
+						if(curr == second){
+							if(!gsv[11].isEmpty()){
+								sats.get(i).setSnr(Integer.parseInt(gsv[11]));									
+							}
+						}
+						if(curr == third){
+							if(!gsv[15].isEmpty()){
+								sats.get(i).setSnr(Integer.parseInt(gsv[15]));							
+							}
+						}
+						if(curr == fourth){
+							if(!finalone[0].isEmpty()){
+								sats.get(i).setSnr(Integer.parseInt(finalone[0]));
+							}
+						}
+
+					}*/
+
+
+
 
 
 
@@ -81,6 +163,7 @@ public class Stream {
 				else if(p[0].equalsIgnoreCase("$GPGSA")){
 
 					String[] gsa = sCurrentLine.toString().split(",");
+
 
 					for(int i = 3;i < gsa.length - 3;i++){
 						if(!gsa[i].isEmpty()){		
@@ -150,16 +233,24 @@ public class Stream {
 		for(int i =0;i<ggas.size();i++){
 			GGA temp = new GGA();
 			temp = ggas.get(i);
-			System.out.println(temp.getTime() +" " + temp.getLat() + " " +temp.getLon());
+			System.out.println(temp.getTime() +" " + temp.getLat() + " " +temp.getLon()+"------");
 		}
 
 
 	}
 
-	public int countReliable( ){
+	public int countReliable(){
 
-		int j=0;
-		return j;
+		int reliable =0;
+
+		for(int i = 0;i<sats.size();i++){
+			if(sats.get(i).getSnr() >= 30){
+				reliable++;
+				//System.out.println(reliable + " This is reliable count");
+			}
+		}
+
+		return reliable;
 	}
 
 }
